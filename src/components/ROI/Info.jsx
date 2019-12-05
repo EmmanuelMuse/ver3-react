3cf43b384960d94a273e95fe582cc8850915ac45
muse-ghtechlabs <emmanuel.muse@ghtechlabs.com>
2019-12-05 14:36:12 -0500

    minor text tweaks karen wanted

--- src/components/ROI/Info.jsx
+++ src/components/ROI/Info.jsx
@@ -12,7 +12,7 @@ return(
 <section className="form">
   <form className="form-box">
     <div className="form-group">
-      <label htmlFor="tbContactName" className="info-label">TechBridge Contact Name</label>
+      <label htmlFor="tbContactName" className="info-label">Contact Name</label>
       <input type="text" className="form-control" id="tbContactName" />
     </div>
     <div className="form-group">
@@ -28,7 +28,7 @@ return(
       <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
     </div>
     <div className="form-group">
-      <label htmlFor="tel" className="info-label">Org. Phone Number</label>
+      <label htmlFor="tel" className="info-label">Phone Number</label>
       <input type="tel" className="form-control" placeholder="XXX-XXX-XXXX" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" id="telNum" />
     </div>
     <div className="nav-button">
