5cb4abb97a4ea959feda1afb9a1f5578e141626c
Briana Peppers <briana.peppers@ghtechlabs.com>
2019-12-12 17:16:10 -0500

    react-router doesn't like gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sass/main.sass';
 //react router
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route} from 'react-router-dom';
 import ScrollToTop from 'react-router-scroll-top'
 //importing pages for Router
 import Home from './components/Home';
@@ -13,17 +13,17 @@ import Results from './components/ROI/results';
 
 function App() {
   return (
-    <BrowserRouter basename= "/ver3-react" >
-      <ScrollToTop>
-        {/* Pages */}
-      <Route exact path="/" component={Home}/>
-      <Route path="/Info" component={Info}/>
-      <Route path="/HoursSaved" component={HourSaved}/>
-      <Route path="/DollarsSaved" component={DollarsSaved}/>
-      <Route path="/ClientsServed" component={clientsServed}/>
-      <Route path="/Results" component={Results}/>
-      </ScrollToTop>
-    </BrowserRouter>
+    <HashRouter basename= "/ver3-react" >
+        <ScrollToTop>
+            {/* Pages */}
+            <Route exact path="/" component={Home}/>
+            <Route path="/Info" component={Info}/>
+            <Route path="/HoursSaved" component={HourSaved}/>
+            <Route path="/DollarsSaved" component={DollarsSaved}/>
+            <Route path="/ClientsServed" component={clientsServed}/>
+            <Route path="/Results" component={Results}/>
+        </ScrollToTop>
+    </HashRouter>
     
     
   );
